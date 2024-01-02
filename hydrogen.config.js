6948ef9206c3d31faa26a44dd0171f98e213feba
senseykaa <senseyika@gmail.com>
2024-01-02 15:16:01 +0200

    first commit

--- hydrogen.config.js
+++ hydrogen.config.js
@@ -6,11 +6,11 @@ export default defineConfig({
     defaultLanguageCode: 'EN',
     storeDomain:
       // @ts-ignore
-      Oxygen?.env?.PUBLIC_STORE_DOMAIN || process.env.PUBLIC_STORE_DOMAIN,
+      Oxygen?.env?.PUBLIC_STORE_DOMAIN || 'a774e7-4.myshopify.com',
     storefrontToken:
       // @ts-ignore
       Oxygen?.env?.PUBLIC_STOREFRONT_API_TOKEN ||
-      process.env.PUBLIC_STOREFRONT_API_TOKEN,
+      'd083ba9d851e77fece0de9fa90f63ea0',
     privateStorefrontToken:
       // @ts-ignore
       Oxygen?.env?.PRIVATE_STOREFRONT_API_TOKEN,
