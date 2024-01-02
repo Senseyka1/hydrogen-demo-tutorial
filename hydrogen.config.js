5c94e5e076768964116cede66da5b6c7277663cd
senseykaa <senseyika@gmail.com>
2024-01-02 16:07:21 +0200

    fix env

--- hydrogen.config.js
+++ hydrogen.config.js
@@ -4,21 +4,12 @@ export default defineConfig({
   shopify: {
     defaultCountryCode: 'US',
     defaultLanguageCode: 'EN',
-    storeDomain:
-      // @ts-ignore
-      Oxygen?.env?.PUBLIC_STORE_DOMAIN ||
-      import.meta.env.VITE_PUBLIC_STORE_DOMAIN,
-    storefrontToken:
-      // @ts-ignore
-      Oxygen?.env?.PUBLIC_STOREFRONT_API_TOKEN ||
-      import.meta.env.VITE_PUBLIC_STOREFRONT_API_TOKEN,
-    privateStorefrontToken:
-      // @ts-ignore
-      Oxygen?.env?.PRIVATE_STOREFRONT_API_TOKEN ||
-      import.meta.env.VITE_PRIVATE_STOREFRONT_API_TOKEN,
+    storeDomain: import.meta.env.VITE_PUBLIC_STORE_DOMAIN,
+    storefrontToken: import.meta.env.VITE_PUBLIC_STOREFRONT_API_TOKEN,
+    privateStorefrontToken: import.meta.env.VITE_PRIVATE_STOREFRONT_API_TOKEN,
     storefrontApiVersion: '2023-04',
     // @ts-ignore
-    storefrontId: Oxygen?.env?.PUBLIC_STOREFRONT_ID,
+    storefrontId: import.meta.env.VITE_PUBLIC_STOREFRONT_ID,
   },
   session: CookieSessionStorage('__session', {
     path: '/',
